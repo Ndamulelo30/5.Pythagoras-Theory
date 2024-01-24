c7d1530236065fe66151d9f372916e2a974b1b9a
Sanele <Sanele@LAPTOP-FC04BNU2>
2024-01-24 14:50:48 +0200

    Add project files.

---
+++ 5.Pythagoras Theory/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5.Pythagoras_Theory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sideA = Convert.ToDouble(textBox1.Text);
+            double sideB = Convert.ToDouble(textBox2.Text);
+
+            //calculate the hypotenuse using pythagoras theory
+            double hypotenuse = Math.Sqrt(Math.Pow(sideA, 2) +  Math.Pow(sideB, 2));
+
+            //display the result
+            textBox3.Text = hypotenuse.ToString ();
+
+
+        }
+    }
+}
